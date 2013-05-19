2a013e37e0613e6167def7966678bb985f3b58d6
gmallard <allard.guy.m@gmail.com>
2013-05-19 08:01:55 -0400

    Copyright updates.

--- jinterop/jsend.sh
+++ jinterop/jsend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2012 Guy M. Allard
+# Copyright © 2012-2013 Guy M. Allard
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
