f1099b2c6f97c19c71d228591eef51bfa875405e
gmallard <allard.guy.m@gmail.com>
2013-10-20 12:40:40 -0400

    Unified publish example.

---
+++ publish/publish.go
@@ -0,0 +1,101 @@
+//
+// Copyright © 2013 Guy M. Allard
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/*
+Publish messages to a STOMP broker.
+
+	Examples:
+
+		# Publish to a broker with all defaults:
+		# Host is "localhost"
+		# Port is 61613
+		# Login is "guest"
+		# Passcode is "guest
+		# Virtual Host is "localhost"
+		# Protocol is 1.1
+		go run publish.go
+
+		# Publish to a broker using STOMP protocol level 1.0:
+		STOMP_PROTOCOL=1.0 go run publish.go
+
+		# Publish to a broker using a custom host and port:
+		STOMP_HOST=tjjackson STOMP_PORT=62613 go run publish.go
+
+		# Publish to a broker using a custom port and virtual host:
+		STOMP_PORT=41613 STOMP_VHOST="/" go run publish.go
+
+		# Publish to a broker using a custom login and passcode:
+		STOMP_LOGIN="userid" STOMP_PASSCODE="t0ps3cr3t" go run publish.go
+
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/gmallard/stompngo"
+	"github.com/gmallard/stompngo_examples/sngecomm"
+	"log"
+	"net"
+)
+
+var exampid = "publish: "
+
+// Connect to a STOMP broker, publish some messages and disconnect.
+func main() {
+	fmt.Println(exampid + "starts ...")
+
+	// Open a net connection
+	h, p := sngecomm.HostAndPort()
+	n, e := net.Dial("tcp", net.JoinHostPort(h, p))
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "dial complete ...")
+
+	ch := sngecomm.ConnectHeaders()
+	conn, e := stompngo.Connect(n, ch)
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid+"stomp connect complete ...", conn.Protocol())
+
+	// *NOTE* your application functionaltiy goes here!
+	s := stompngo.Headers{"destination", sngecomm.Dest()} // send headers
+	m := exampid + " message: "
+	for i := 1; i <= sngecomm.Nmsgs(); i++ {
+		t := m + fmt.Sprintf("%d", i)
+		e := conn.Send(s, t)
+		if e != nil {
+			log.Fatalln(e) // Handle this ...
+		}
+		fmt.Println(exampid, "send complete:", t)
+	}
+
+	// Disconnect from the Stomp server
+	e = conn.Disconnect(stompngo.Headers{})
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "stomp disconnect complete ...")
+	// Close the network connection
+	e = n.Close()
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "network close complete ...")
+
+	fmt.Println(exampid + "ends ...")
+}
