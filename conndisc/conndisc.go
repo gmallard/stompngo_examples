bef5ae5a0a6e1a63bbe0d9c054dcdf0eb3b46d27
gmallard <allard.guy.m@gmail.com>
2013-10-20 12:03:16 -0400

    Unified connect / disconnect example:
    See godoc for detailed usage examples.

---
+++ conndisc/conndisc.go
@@ -0,0 +1,98 @@
+//
+// Copyright © 2013 Guy M. Allard
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/*
+Connect and Disconnect from a STOMP broker using TCP.
+
+	Examples:
+
+		# Connect to a broker with all defaults:
+		# Host is "localhost"
+		# Port is 61613
+		# Login is "guest"
+		# Passcode is "guest
+		# Virtual Host is "localhost"
+		go run conndisc.go
+
+		# Connect to a broker using STOMP protocol level 1.0
+		STOMP_PROTOCOL=1.0 go run conndisc.go
+
+		# Connect to a broker using a custom host and port:
+		STOMP_HOST=tjjackson STOMP_PORT=62613 go run conndisc.go
+
+		# Connect to a broker using a custom port and virtual host:
+		STOMP_PORT=41613 STOMP_VHOST="/" go run conndisc.go
+
+		# Connect to a broker using a custom login and passcode:
+		STOMP_LOGIN="userid" STOMP_PASSCODE="t0ps3cr3t" go run conndisc.go
+
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/gmallard/stompngo"
+	. "github.com/gmallard/stompngo_examples/sngecomm"
+	"log"
+	"net"
+)
+
+var exampid = "connect: "
+
+// Connect to a STOMP broker and disconnect.
+func main() {
+	fmt.Println(exampid + "starts ...")
+
+	// Open a net connection
+	h, p := HostAndPort()
+	n, e := net.Dial("tcp", net.JoinHostPort(h, p))
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "dial complete ...")
+
+	// All stomp API methods require 'Headers'.  Stomp headers are key/value
+	// pairs.  The stompngo package implements them using a string slice.
+	ch := ConnectHeaders()
+
+	// Get a stomp connection.  Parameters are:
+	// a) the opened net connection
+	// b) the Headers
+	conn, e := stompngo.Connect(n, ch)
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "stomp connect complete ...")
+
+	// *NOTE* your application functionaltiy goes here!
+
+	// Polite Stomp disconnects are not required, but highly recommended.
+	// Empty headers here for the DISCONNECT
+	e = conn.Disconnect(stompngo.Headers{})
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "stomp disconnect complete ...")
+
+	// Close the net connection.
+	e = n.Close()
+	if e != nil {
+		log.Fatalln(e) // Handle this ......
+	}
+	fmt.Println(exampid + "network close complete ...")
+
+	fmt.Println(exampid + "ends ...")
+}
