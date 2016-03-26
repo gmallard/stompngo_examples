b6da6e9f85102838ac6353f9fabbd9effa76667b
gmallard <allard.guy.m@gmail.com>
2016-03-26 19:05:33 -0400

    Update some run scripts.

--- sngissue25/client02-hang/run.sh
+++ sngissue25/client02-hang/run.sh
@@ -11,11 +11,11 @@ ncms=10
 proto=1.2
 #
 port=61613 # ActiveMQ Here
-port=62613 # Apollo Here
-port=41613 # RabbitMQ Here
+#port=62613 # Apollo Here
+#port=41613 # RabbitMQ Here
 #
 vh=localhost # OK for AMQ or Apollo
-vh=/ # OK for RMQ
+#vh=/ # OK for RMQ
 #
 STOMP_NMSGS=$npms STOMP_VHOST=$vh STOMP_PORT=$port STOMP_PROTOCOL=$proto STOMP_DEST=/queue/client02-hang.1 go run $basic/publish/publish.go
 STOMP_DRAIN=yes STOMP_NMSGS=$ncms STOMP_VHOST=$vh STOMP_PORT=$port STOMP_PROTOCOL=$proto STOMP_DEST=/queue/client02-hang.1 go run client02-hang.go
