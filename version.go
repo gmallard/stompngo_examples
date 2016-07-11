332324c4bc966f09269f8d66ad8e981cbd82de1a
gmallard <allard.guy.m@gmail.com>
2016-07-11 09:20:33 -0400

    Start next dev round.

--- version.go
+++ version.go
@@ -29,10 +29,10 @@ import (
 )
 
 var (
-	pref  = "v" // Prefix
-	major = "1" // Major
-	minor = "0" // Minor
-	patch = "2" // Patch
+	pref  = "v"          // Prefix
+	major = "1"          // Major
+	minor = "0"          // Minor
+	patch = "2.plvl.001" // Patch
 )
 
 func main() {
